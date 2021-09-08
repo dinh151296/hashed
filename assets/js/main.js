cc1853c444ca59ce81b23b93c17aed1929fdb152
Thanh Dinh <mr.thanhdinh1996@gmail.com>
2021-09-09 01:13:23 +0700

    new cm

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,29 +18,3 @@ burgerElement.addEventListener('click', function() {
     }
 });
 
-
-//hover page portfolio
-
-const elementHover = document.querySelectorAll('.event-hover');
-
-for (let index = 0; index < elementHover.length; index++) {
-
-    // const contentShowed = document.querySelector('.show');
-    // console.log(contentShowed);
-
-    elementHover[index].addEventListener('mouseenter', function() {
-        const parentE = this.parentElement.parentElement;
-        const contentShow = parentE.children[1];
-        console.log(contentShow);
-
-        
-
-        if(contentShow.classList.contains('show')) {
-            contentShow.classList.remove('show');
-            console.log("cos");
-        } else {
-            contentShow.classList.add('show');
-        }
-        
-    });
-}
